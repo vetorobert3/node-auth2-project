96e94a03e46a72a6c6388c0930923873374269a9
Veto Ramirez <vetorobert3@gmail.com>
2020-06-09 20:05:14 -0700

    working on users

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -3,7 +3,7 @@ const secrets = require('../config/secrets.js')
 
 module.exports = (req, res, next) => {
   // add code here to verify users are logged in
-  const [directive, token] = req.headers.authorization.split(' ');
+  const [token, directive] = req.headers.authorization.split(" ");
   
   if (!directive || directive != 'bearer') {
     res.status(401).json({ learn: 'to type '});
@@ -23,3 +23,29 @@ module.exports = (req, res, next) => {
     res.status(401).json({you: 'shall not pass'});
   }
 };
+
+// const jwt = require('jsonwebtoken');
+// const secrets = require('../config/secrets.js');
+
+// module.exports = (req, res, next) => {
+
+//   try {
+//     const token = req.headers.authorization.split(" ")[1];
+
+//     if (token) {
+//       jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+//         if (err) {
+//           res.status(401).json({ you: "can't touch this" });
+//         } else {
+//           req.decodedJwt = decodedToken;
+//           console.log(req.decodedJwt);
+//           next();
+//         }
+//       })
+//     } else {
+//       throw new Error('invalid auth data');
+//     }
+//   } catch (err) {
+//     res.status(401).json({ error: err.message });
+//   }
+// };
--- users/users-model.js
+++ users/users-model.js
@@ -8,16 +8,16 @@ module.exports = {
 };
 
 function find() {
-  return db("users").select("id", "username").orderBy("id");
+  return db("user").select("id", "username").orderBy("id");
 }
 
 function findBy(filter) {
-  return db("users").where(filter).orderBy("id");
+  return db("user").where(filter).orderBy("id");
 }
 
 async function add(user) {
   try {
-    const [id] = await db("users").insert(user, "id");
+    const [id] = await db("user").insert(user, "id");
 
     return findById(id);
   } catch (error) {
@@ -26,5 +26,5 @@ async function add(user) {
 }
 
 function findById(id) {
-  return db("users").where({ id }).first();
+  return db("user").where({ id }).first();
 }
